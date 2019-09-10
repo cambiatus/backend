995c964bb270db5c09b73c5c531b78c080d4ee80
Lucca <julienlucca@gmail.com>
2019-09-10 11:58:54 -0300

    Version reset

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BeSpiral.Mixfile do
   def project do
     [
       app: :bespiral,
-      version: "0.35.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
