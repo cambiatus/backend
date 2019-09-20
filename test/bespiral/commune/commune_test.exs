8cbe4680aad92072cd968154353de73581c4e953
Zacck Osiemo <coderv63@gmail.com>
2019-09-20 07:50:45 +0200

    Refactor unit test for arrangement and utilize ex_machina for building fixtures

--- test/bespiral/commune/commune_test.exs
+++ test/bespiral/commune/commune_test.exs
@@ -2,12 +2,11 @@ defmodule BeSpiral.CommuneTest do
   use BeSpiral.DataCase
 
   alias BeSpiral.{
-    Commune
+    Commune,
+    Commune.Community
   }
 
   describe "communities" do
-    alias BeSpiral.Commune.Community
-
     @valid_attrs %{
       symbol: "TES",
       issuer: "testtesttest",
@@ -25,22 +24,13 @@ defmodule BeSpiral.CommuneTest do
     @update_attrs %{name: "some updated name"}
     @invalid_attrs %{symbol: nil}
 
-    def community_fixture(attrs \\ %{}) do
-      {:ok, community} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Commune.create_community()
-
-      community
-    end
-
     test "list_communities/0 returns all communities" do
-      community = community_fixture()
+      community = insert(:community)
       assert Commune.list_communities() == {:ok, [community]}
     end
 
     test "get_community!/1 returns the community with given symbol" do
-      community = community_fixture()
+      community = insert(:community)
       assert Commune.get_community!(community.symbol) == community
     end
 
@@ -54,26 +44,21 @@ defmodule BeSpiral.CommuneTest do
     end
 
     test "update_community/2 with valid data updates the community" do
-      community = community_fixture()
+      community = insert(:community)
       assert {:ok, %Community{} = community} = Commune.update_community(community, @update_attrs)
       assert community.name == "some updated name"
     end
 
     test "update_community/2 with invalid data returns error changeset" do
-      community = community_fixture()
+      community = insert(:community)
       assert {:error, %Ecto.Changeset{}} = Commune.update_community(community, @invalid_attrs)
       assert community == Commune.get_community!(community.symbol)
     end
 
     test "delete_community/1 deletes the community" do
-      community = community_fixture()
+      community = insert(:community)
       assert {:ok, %Community{}} = Commune.delete_community(community)
       assert_raise Ecto.NoResultsError, fn -> Commune.get_community!(community.symbol) end
-    end
-
-    test "change_community/1 returns a community changeset" do
-      community = community_fixture()
-      assert %Ecto.Changeset{} = Commune.change_community(community)
     end
   end
 
@@ -82,24 +67,20 @@ defmodule BeSpiral.CommuneTest do
 
     @invalid_attrs %{account: nil, community: nil, invited_by: nil}
 
-    def network_fixture(attrs \\ %{}) do
-      {:ok, network} = Commune.create_network(attrs)
-      network
-    end
-
     test "list_community_network/1 returns community network", %{
       community: community,
       user: user,
       another_user: another_user
     } do
       network =
-        network_fixture(%{
-          account_id: another_user.account,
-          community_id: community.symbol,
-          invited_by_id: user.account
+        insert(:network, %{
+          account: another_user,
+          community: community,
+          invited_by: user
         })
 
-      assert Commune.list_community_network(community.symbol) == [network]
+      assert [net] = Commune.list_community_network(community.symbol)
+      assert net.id == network.id
     end
 
     test "list_network/0 returns all network", %{
@@ -108,13 +89,14 @@ defmodule BeSpiral.CommuneTest do
       another_user: another_user
     } do
       network =
-        network_fixture(%{
-          account_id: another_user.account,
-          community_id: community.symbol,
-          invited_by_id: user.account
+        insert(:network, %{
+          account: another_user,
+          community: community,
+          invited_by: user
         })
 
-      assert Commune.list_network() == [network]
+      assert [net] = Commune.list_network()
+      assert net.id == network.id
     end
 
     test "get_network!/1 returns the network with given id", %{
@@ -123,13 +105,14 @@ defmodule BeSpiral.CommuneTest do
       another_user: another_user
     } do
       network =
-        network_fixture(%{
-          account_id: another_user.account,
-          community_id: community.symbol,
-          invited_by_id: user.account
+        insert(:network, %{
+          account: another_user,
+          community: community,
+          invited_by: user
         })
 
-      assert Commune.get_network!(network.id) == network
+      assert net = Commune.get_network!(network.id)
+      assert net.id == network.id
     end
 
     test "create_network/1 with valid data creates a network", %{
@@ -138,12 +121,14 @@ defmodule BeSpiral.CommuneTest do
       another_user: another_user
     } do
       network =
-        %{
-          account_id: another_user.account,
-          community_id: community.symbol,
-          invited_by_id: user.account
-        }
-        |> network_fixture()
+        insert(
+          :network,
+          %{
+            account: another_user,
+            community: community,
+            invited_by: user
+          }
+        )
         |> Repo.preload(:community)
         |> Repo.preload(:account)
         |> Repo.preload(:invited_by)
@@ -155,21 +140,6 @@ defmodule BeSpiral.CommuneTest do
 
     test "create_network/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Commune.create_network(@invalid_attrs)
-    end
-
-    test "change_network/1 returns a network changeset", %{
-      community: community,
-      user: user,
-      another_user: another_user
-    } do
-      network =
-        network_fixture(%{
-          account_id: another_user.account,
-          community_id: community.symbol,
-          invited_by_id: user.account
-        })
-
-      assert %Ecto.Changeset{} = Commune.change_network(network)
     end
   end
 end
