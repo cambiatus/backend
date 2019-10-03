02274262ae8454864eb56362fa615411dfbd1be7
Zacck Osiemo <coderv63@gmail.com>
2019-10-03 10:40:45 +0200

    Test that we can count unread notifications

--- test/bespiral_web/resolvers/notifications_test.exs
+++ test/bespiral_web/resolvers/notifications_test.exs
@@ -5,7 +5,8 @@ defmodule BeSpiralWeb.Resolvers.NotificationsTests do
   use BeSpiral.ApiCase
 
   alias BeSpiral.{
-    Notifications.PushSubscription
+    Notifications.PushSubscription,
+    Notifications.NotificationHistory
   }
 
   describe "Notifications resolvers" do
@@ -42,6 +43,51 @@ defmodule BeSpiralWeb.Resolvers.NotificationsTests do
       assert Repo.aggregate(PushSubscription, :count, :id) == 1
 
       assert pushed_account["accountId"] == user.account
+    end
+
+    @num 3
+    test "collects number of unread notifications for a user", %{conn: conn} do
+      assert Repo.aggregate(NotificationHistory, :count, :id) == 0
+
+      user = insert(:user)
+
+      _ =
+        1..@num
+        |> Enum.map(fn _ ->
+          insert(:notification_history, %{is_read: true, recipient: user})
+        end)
+
+      unread_histories =
+        1..@num
+        |> Enum.map(fn _ ->
+          insert(:notification_history, %{recipient: user})
+        end)
+
+      assert Repo.aggregate(NotificationHistory, :count, :id) == @num * 2
+
+      query = """
+      query($input: UnreadNotificationsInput!) {
+        unreadNotifications(input: $input) {
+          count 
+        }
+      }
+      """
+
+      variables = %{
+        "input" => %{
+          "account" => user.account
+        }
+      }
+
+      res = conn |> post("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "unreadNotifications" => unread
+        }
+      } = json_response(res, 200)
+
+      assert unread["count"] == Enum.count(unread_histories)
     end
   end
 end
