7028f1c5d4e8ae8d2c8cc80b2b438b63bd7dff1e
Lucca <julienlucca@gmail.com>
2019-10-03 13:13:17 -0300

    remove empty test file

--- test/bespiral/notifications/notification_history_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule BeSpiral.NotificationHistoryTest do
-  use BeSpiral.DataCase
-
-  describe "notification_history" do
-    test "collects a notification when one exists"
-    test "collects all notifications for a user"
-  end
-end
