edd9ee841b3e9cc0178ac1f3bff4076778930bf9
Zacck Osiemo <coderv63@gmail.com>
2019-10-08 08:55:12 +0200

    Build trigger and function to monitor claim checks

---
+++ priv/repo/migrations/20191008061455_create_validation_function.exs
@@ -0,0 +1,31 @@
+defmodule BeSpiral.Repo.Migrations.CreateValidationFunction do
+  @moduledoc """
+  Function to create a trigger when a claim validation is added to the database
+  """
+  use Ecto.Migration
+
+  @function "validation_notification"
+  @event "check_added"
+
+  def up do
+    execute("""
+      CREATE OR REPLACE FUNCTION #{@function}()
+      RETURNS trigger AS $$ 
+      BEGIN 
+        PERFORM pg_notify(
+          '#{@event}',
+          json_build_object(
+            'operation', TG_OP,
+            'record', row_to_json(NEW)
+            )::text
+        );
+        RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+    """)
+  end
+
+  def down do
+    execute("DROP FUNCTION IF EXISTS #{@function} CASCADE")
+  end
+end
