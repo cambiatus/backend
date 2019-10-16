9ad2d533399ee0a04fd3be61eeec34c17a7b3ff3
Zacck Osiemo <coderv63@gmail.com>
2019-10-16 11:54:21 +0200

    Add test case to use when testing subscriptions

---
+++ test/support/subscription_case.ex
@@ -0,0 +1,38 @@
+defmodule BeSpiralWeb.SubscriptionCase do
+  @moduledoc """
+  A test case for testing Absinter Subscriptions 
+  """
+
+  use ExUnit.CaseTemplate
+  use Phoenix.ChannelTest
+
+  @endpoint BeSpiralWeb.Endpoint
+  alias BeSpiralWeb.UserSocket
+
+  using do
+    quote do
+      use Phoenix.ChannelTest
+
+      use Absinthe.Phoenix.SubscriptionTest,
+        schema: BeSpiralWeb.Schema
+
+      @endpoint BeSpiralWeb.Endpoint
+
+      import BeSpiral.Factory
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(BeSpiral.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(BeSpiral.Repo, {:shared, self()})
+    end
+
+    {:ok, socket} = Phoenix.ChannelTest.connect(UserSocket, %{})
+
+    {:ok, socket} = Absinthe.Phoenix.SubscriptionTest.join_absinthe(socket)
+
+    {:ok, socket: socket}
+  end
+end
