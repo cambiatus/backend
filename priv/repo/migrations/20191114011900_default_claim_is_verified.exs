3211d79594ab7fb708d624a294fc9e0a8967666a
Lucca <julienlucca@gmail.com>
2019-11-13 22:25:31 -0300

    Add default value for claims table, column `is_verified`

---
+++ priv/repo/migrations/20191114011900_default_claim_is_verified.exs
@@ -0,0 +1,10 @@
+defmodule BeSpiral.Repo.Migrations.DefaultClaimIsVerified do
+  use Ecto.Migration
+
+  def change do
+    alter table(:claims) do
+      modify(:is_verified, :bool, default: false)
+    end
+
+  end
+end
