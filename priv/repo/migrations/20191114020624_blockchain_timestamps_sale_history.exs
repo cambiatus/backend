47e837fcdaeff381f37a6960af86414c8e582a6f
Julien Lucca <julienlucca@gmail.com>
2019-11-14 06:57:37 -0300

    Feature/blockchain timestamps sale history (#18)
    * Add blockchain timestamps to sale_history table
    
    * Make sure it modify sale_history table

---
+++ priv/repo/migrations/20191114020624_blockchain_timestamps_sale_history.exs
@@ -0,0 +1,12 @@
+defmodule BeSpiral.Repo.Migrations.BlockchainTimestampsSaleHistory do
+  use Ecto.Migration
+
+  def change do
+    alter table(:sale_history) do
+      add(:created_block, :integer)
+      add(:created_tx, :string)
+      add(:created_eos_account, :string)
+      add(:created_at, :utc_datetime)
+    end
+  end
+end
