21ccf300ff7b0ff85b29d56ad0580d7a8f6c4c81
Lucca <julienlucca@gmail.com>
2020-03-17 13:06:47 -0300

    Remove error handling from chat

--- lib/cambiatus_web/views/auth_view.ex
+++ lib/cambiatus_web/views/auth_view.ex
@@ -34,37 +34,4 @@ defmodule CambiatusWeb.AuthView do
   def render("unauthorized.json", _) do
     %{errors: %{details: "User already registered. Please try signing in instead."}}
   end
-
-  def render("chat_error.json", %{error: error}) do
-    case error do
-      {:error, :chat_signin_bad_request} ->
-        %{
-          errors: %{
-            details: "Chat error during sign in. Looks like data aren't in the correct format"
-          }
-        }
-
-      {:error, :chat_signin_unauthorized} ->
-        %{errors: %{details: "Chat error during sign in. Are the credentials right?"}}
-
-      {:error, :chat_signin_unknown_error} ->
-        %{errors: %{details: "Chat error during sign in. Unknown error :("}}
-
-      {:error, :chat_signup_bad_request} ->
-        %{
-          errors: %{
-            details: "Chat error during sign up. Looks like data aren't in the correct format"
-          }
-        }
-
-      {:error, :chat_signup_unauthorized} ->
-        %{errors: %{details: "Chat error during sign in. Are the credentials right?"}}
-
-      {:error, :chat_signup_unknown_error} ->
-        %{errors: %{details: "Chat error during sign up. Unknown error :("}}
-
-      {:error, _} ->
-        %{errors: %{details: "Chat unknown error :("}}
-    end
-  end
 end
