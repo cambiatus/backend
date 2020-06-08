c0f4765c73af1fa67484c77a479e11365f176512
Andrey Miskov <amiskov@gmail.com>
2020-06-08 16:24:36 +0300

    Add tests

--- test/cambiatus_web/schema/resolvers/accounts_test.exs
+++ test/cambiatus_web/schema/resolvers/accounts_test.exs
@@ -5,7 +5,8 @@ defmodule CambiatusWeb.Schema.Resolvers.AccountsTest do
   use Cambiatus.ApiCase
 
   alias Cambiatus.{
-    Accounts.User
+    Accounts.User,
+    Commune.Transfer
   }
 
   describe "Accounts Resolver" do
@@ -73,6 +74,281 @@ defmodule CambiatusWeb.Schema.Resolvers.AccountsTest do
       assert Repo.aggregate(User, :count, :account) == 1
       assert profile["account"] == usr.account
       refute profile["bio"] == usr.bio
+    end
+  end
+
+  describe "payment history" do
+    setup do
+      assert Repo.aggregate(User, :count, :account) == 0
+      assert Repo.aggregate(Transfer, :count, :id) == 0
+
+      utc_today = NaiveDateTime.utc_now()
+      utc_yesterday = NaiveDateTime.add(utc_today, -(24 * 60 * 60))
+
+      user1 = insert(:user, %{account: "user1"})
+      user2 = insert(:user, %{account: "user2"})
+
+      transfers = [
+        # user1 -> user2
+        insert(:transfer, %{from: user1, to: user2, created_at: utc_today}),
+        insert(:transfer, %{from: user1, to: user2, created_at: utc_today}),
+        insert(:transfer, %{from: user1, to: user2, created_at: utc_yesterday}),
+
+        # user1 <- user2
+        insert(:transfer, %{from: user2, to: user1, created_at: utc_today})
+      ]
+
+      assert Repo.aggregate(User, :count, :account) == 2
+      assert Repo.aggregate(Transfer, :count, :id) == 4
+
+      %{
+        :users => [user1, user2],
+        :transfers => transfers,
+        :variables => %{
+          "input" => %{
+            "account" => user1.account # tests are based on the `user1` profile
+          },
+          "first" => Enum.count(transfers)
+        }
+      }
+    end
+
+    test "incoming transfers", %{conn: conn, variables: variables} do
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(first: $first, direction: INCOMING) {
+              fetchedCount
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => user1_incoming_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      assert user1_incoming_transfers == 1
+    end
+
+    test "outgoing transfers", %{conn: conn, variables: variables} do
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(first: $first, direction: OUTGOING) {
+              fetchedCount
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => user1_outgoing_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      assert user1_outgoing_transfers == 3
+    end
+
+    test "transfers for the date", %{conn: conn, variables: variables} do
+      today_date = Date.to_string(Date.utc_today)
+
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(first: $first, date: "#{today_date}") {
+              fetchedCount
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => user1_today_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      assert user1_today_transfers == 3
+    end
+
+    test "incoming transfers for the date", %{conn: conn, variables: variables} do
+      today_date = Date.to_string(Date.utc_today)
+
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(first: $first, direction: INCOMING, date: "#{today_date}") {
+              fetchedCount
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => user1_today_incoming_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      assert user1_today_incoming_transfers == 1
+    end
+
+    test "outgoing transfers for the date", %{conn: conn, variables: variables} do
+      today_date = Date.to_string(Date.utc_today)
+
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(first: $first, direction: OUTGOING, date: "#{today_date}") {
+              fetchedCount
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => user1_today_outgoing_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      assert user1_today_outgoing_transfers == 2
+    end
+
+    test "incoming transfers for the date from user2 to user1", %{conn: conn, users: users, variables: variables} do
+      today_date = Date.utc_today |> Date.to_string
+
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(
+              first: $first,
+              direction: INCOMING,
+              secondPartyAccount: "user2",
+              date: "#{today_date}"
+            ) {
+              fetchedCount
+              edges {
+                node {
+                  from {
+                    account
+                  }
+                  to {
+                    account
+                  }
+                }
+              }
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => transfers_from_user2_to_user1_for_today_count,
+              "edges" => collected_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      get_account = &(&1["node"][&2]["account"])
+
+      assert Enum.all?(
+               collected_transfers,
+               fn t -> get_account.(t, "from") == "user2" && get_account.(t, "to") == "user1" end
+             ) == true
+
+      assert transfers_from_user2_to_user1_for_today_count == 1
+    end
+
+    test "outgoing transfers for the date from user1 to user2", %{conn: conn, users: users, variables: variables} do
+      today_date = Date.utc_today |> Date.to_string
+
+      query = """
+        query ($input: ProfileInput!, $first: Int!) {
+          profile(input: $input) {
+            transfers(
+              first: $first,
+              direction: OUTGOING,
+              secondPartyAccount: "user2",
+              date: "#{today_date}"
+            ) {
+              fetchedCount
+              edges {
+                node {
+                  from {
+                    account
+                  }
+                  to {
+                    account
+                  }
+                }
+              }
+            }
+          }
+        }
+      """
+
+      res = conn |> get("/api/graph", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "profile" => %{
+            "transfers" => %{
+              "fetchedCount" => transfers_from_user1_to_user2_for_today_count,
+              "edges" => collected_transfers
+            }
+          }
+        }
+      } = json_response(res, 200)
+
+      get_account = &(&1["node"][&2]["account"])
+
+      assert Enum.all?(
+               collected_transfers,
+               fn t -> get_account.(t, "from") == "user1" && get_account.(t, "to") == "user2" end
+             ) == true
+
+      assert transfers_from_user1_to_user2_for_today_count == 2
     end
   end
 end
