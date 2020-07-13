c89ee7622ad697ef208579bdad0da143db1f06dd
Rodrigo Leite <rodrigo@leite.dev>
2020-07-13 14:26:44 -0300

    Extract save functionality

--- lib/cambiatus_web/controllers/s3_contoller.ex
+++ lib/cambiatus_web/controllers/s3_contoller.ex
@@ -1,19 +1,31 @@
 defmodule CambiatusWeb.S3Controller do
   use CambiatusWeb, :controller
 
+  defp save_file(file_path, filename) do
+    bucket_name = System.get_env("BUCKET_NAME")
+    file_uuid = UUID.uuid4(:hex)
+    image_filename = filename
+    unique_filename = "#{file_uuid}-#{image_filename}"
+    {:ok, image_binary} = File.read(file_path)
+
+    operation = ExAws.S3.put_object(bucket_name, "/#{unique_filename}", image_binary)
+
+    case ExAws.request(operation) do
+      {:ok, _} ->
+        {:ok, "https://#{bucket_name}.s3.amazonaws.com/#{bucket_name}/#{unique_filename}"}
+
+      _ ->
+        {:error}
+    end
+  end
+
   def save(conn, params) do
-    with %{path: file_path, filename: filename} = Map.get(params, "file") do
-      bucket_name = System.get_env("BUCKET_NAME")
-      file_uuid = UUID.uuid4(:hex)
-      image_filename = filename
-      unique_filename = "#{file_uuid}-#{image_filename}"
-      {:ok, image_binary} = File.read(file_path)
-
-      ExAws.S3.put_object(bucket_name, unique_filename, image_binary)
-      |> ExAws.request!()
-
-      json(conn, %{
-        data: "https://#{bucket_name}.s3.amazonaws.com/#{bucket_name}/#{unique_filename}"
+    with %{path: file_path, filename: filename} <- Map.get(params, "file"),
+         {:ok, url} <- save_file(file_path, filename) do
+      conn
+      |> put_status(200)
+      |> json(%{
+        data: url
       })
     else
       nil ->
