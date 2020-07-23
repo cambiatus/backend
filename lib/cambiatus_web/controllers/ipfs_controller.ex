44ea9ccfe6283202eb12702a93d9581d78719b66
Rodrigo Leite <rodrigo@leite.dev>
2020-07-23 13:52:31 -0300

    Redirect IPFS route to new upload route

--- lib/cambiatus_web/controllers/ipfs_controller.ex
+++ lib/cambiatus_web/controllers/ipfs_controller.ex
@@ -2,25 +2,8 @@ defmodule CambiatusWeb.IPFSController do
   use CambiatusWeb, :controller
 
   def save(conn, params) do
-    [conn: ipfs] = :cambiatus |> Application.get_env(:ipfs)
-
-    ipfs_conn =
-      IPFS.API.conn()
-      |> Map.merge(ipfs)
-
-    with %{path: file_path} = Map.get(params, "file"),
-         {:ok, %{hash: hash}} <- IPFS.API.add(ipfs_conn, file_path) do
-      json(conn, %{data: %{hash: hash}})
-    else
-      nil ->
-        conn
-        |> put_status(422)
-        |> json(%{error: "File is required"})
-
-      _ ->
-        conn
-        |> put_status(500)
-        |> json(%{error: "Something went wrong while uploading your image"})
-    end
+    conn
+    |> put_status(:moved_permanently)
+    |> redirect(to: CambiatusWeb.UploadController.save(conn, params))
   end
 end
