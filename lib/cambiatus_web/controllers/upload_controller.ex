dfd69be3e138cdf697f23bddf57d1c4380093a2d
Rodrigo Leite <rodrigo@leite.dev>
2020-07-24 11:52:30 -0300

    Deprecate IPFS endpoint for S3 (#51)

---
+++ lib/cambiatus_web/controllers/upload_controller.ex
@@ -0,0 +1,28 @@
+defmodule CambiatusWeb.UploadController do
+  use CambiatusWeb, :controller
+
+  alias Cambiatus.Upload
+
+  def save(conn, params) do
+    with %{path: file_path} <- Map.get(params, "file"),
+         file_info <- File.lstat!(file_path),
+         file_contents <- File.read!(file_path),
+         {:ok, url} <- Upload.save(file_info, file_contents) do
+      conn
+      |> put_status(200)
+      |> json(%{
+        data: url
+      })
+    else
+      nil ->
+        conn
+        |> put_status(422)
+        |> json(%{error: "File is required"})
+
+      {:error, err} ->
+        conn
+        |> put_status(500)
+        |> json(%{error: err})
+    end
+  end
+end
