86e6d2ae904ad86ff34aba63b3b2ea5af5374f57
Rodrigo Leite <rodrigo@leite.dev>
2020-09-07 10:57:18 -0300

    Add content-type information when uploading to S3

--- lib/cambiatus_web/controllers/upload_controller.ex
+++ lib/cambiatus_web/controllers/upload_controller.ex
@@ -4,10 +4,11 @@ defmodule CambiatusWeb.UploadController do
   alias Cambiatus.Upload
 
   def save(conn, params) do
-    with %{path: file_path} <- Map.get(params, "file"),
+    with %{path: file_path, content_type: content_type} <-
+           Map.get(params, "file"),
          file_info <- File.lstat!(file_path),
          file_contents <- File.read!(file_path),
-         {:ok, url} <- Upload.save(file_info, file_contents) do
+         {:ok, url} <- Upload.save(file_info, content_type, file_contents) do
       conn
       |> put_status(200)
       |> json(%{
