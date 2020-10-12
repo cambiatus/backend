9b66ced3afd6a6168c7b459cace67aa26d64d42e
newdev42 <newdev42@gmail.com>
2020-10-12 18:42:09 -0400

    adds columns to actions and claims

---
+++ priv/repo/migrations/20201012162709_add_proof_actions.exs
@@ -0,0 +1,16 @@
+defmodule Cambiatus.Repo.Migrations.AddKycFeatureCommunities do
+  use Ecto.Migration
+
+  def change do
+    alter table("actions") do
+      add(:has_proof_photo, :boolean, default: false)
+      add(:has_proof_code, :boolean, default: false)
+      add(:photo_proof_instructions, :string, null: true)
+    end
+
+    alter table("claims") do
+      add(:proof_photo, :string, null: true)
+      add(:proof_code, :string, null: true)
+    end
+  end
+end
