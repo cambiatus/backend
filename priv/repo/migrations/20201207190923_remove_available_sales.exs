7cb6468db9da9a2dc7c7ab6d9a2b563eaaa0639f
Lucca <julienlucca@gmail.com>
2020-12-08 19:24:42 -0300

    Fix migration error

--- priv/repo/migrations/20201207190923_remove_available_sales.exs
+++ priv/repo/migrations/20201207190923_remove_available_sales.exs
@@ -2,6 +2,16 @@ defmodule Cambiatus.Repo.Migrations.RemoveAvailableSales do
   use Ecto.Migration
 
   def change do
-    execute("DROP MATERIALIZED VIEW available_sales")
+    execute("""
+    DROP MATERIALIZED VIEW IF EXISTS available_sales;
+    """)
+
+    execute("""
+    DROP FUNCTION IF EXISTS refresh_available_sales() CASCADE;
+    """)
+
+    execute("""
+    DROP TRIGGER IF EXISTS refresh_available_sales_trigger ON sales;
+    """)
   end
 end
