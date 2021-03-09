176828e648f8abc404528a9f2f6a402393ed1e1d
venomnert <neerthigan@gmail.com>
2021-03-09 00:30:50 -0500

    Ignored iex to resolve local iex issue

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,5 @@
-alias Cambiatus.{Repo, Error}
-alias Cambiatus.Accounts.{User, Contacts}
-alias Ecto.Changeset
+# alias Cambiatus.{Repo, Error}
+# alias Cambiatus.Accounts.{User, Contacts}
+# alias Ecto.Changeset
 
-lucca = Repo.get(User, "lucca")
+# lucca = Repo.get(User, "lucca")
