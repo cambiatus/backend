8d8f9a96d538ca0cac0a1709ce2b0332e0a8a228
venomnert <neerthigan@gmail.com>
2021-03-10 02:47:53 -0500

    Fixed account seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,7 +1,7 @@
 IO.puts("Running Seeds...")
 
 # Insert Cambiatus user
-data = %{account: "cambiatus"}
+data = %{account: "cambiatus123", name: "cambiatus", email: "test@gmail.com"}
 changeset = Cambiatus.Accounts.User.changeset(%Cambiatus.Accounts.User{}, data)
 {:ok, _} = Cambiatus.Repo.insert(changeset)
 
