6159ec690845a986b3d44df850b4f50307b3833f
venomnert <neerthigan@gmail.com>
2021-03-18 02:00:45 -0400

    Implemented verification utilizing port

--- lib/cambiatus/auth/auth.ex
+++ lib/cambiatus/auth/auth.ex
@@ -22,23 +22,6 @@ defmodule Cambiatus.Auth do
   # """
   def sign_in(account, password) do
     password = "d8Ed.-qfhj7"
-    account
-    |> Accounts.get_user()
-    |> case do
-      nil ->
-        {:error, :not_found}
-
-      user ->
-        if Accounts.verify_pass(account, password) do
-          {:ok, user}
-        else
-          {:error, :invalid_password}
-        end
-    end
-  end
-
-  def sign_in_v2(account, password) do
-    # valid = verifyHash(password, sha256(@static_phrase), publicKeyFromAccount(account)) |> IO.inspect(label: "RESULT")
 
     account
     |> Accounts.get_user()
@@ -55,19 +38,71 @@ defmodule Cambiatus.Auth do
     end
   end
 
-  # TODO create different encodin based the encoding used on frontend: utf8, hex or base64 see (eosjs-ecc/src/api_common.js)
-  def sha256(data, encoding \\ :hex) do
-    :crypto.hash(:sha256, data)
-    |> Base.encode16(case: :lower)
+  def sign_in_v2(account, signature) do
+    verify_signature(signature)
+    |> case do
+      true ->
+        account
+        |> Accounts.get_user()
+        |> case do
+          nil ->
+            {:error, :not_found}
+
+          user ->
+            if Accounts.verify_pass(account, signature) do
+              {:ok, user}
+            else
+              {:error, :invalid_password}
+            end
+        end
+
+      false ->
+        {:error, "Invalid signature"}
+    end
   end
 
-  # signature has to be string
-  defp verify(signature, phrase, pubkey) do
-    verify_hash(signature, __MODULE__.sha256(phrase), pubkey)
-  end
+  defp verify_signature(signature) do
+    {:ok, message} = Jason.encode(%{message: "hello"})
 
-  defp verify_hash(signature, dataSha256, pubkey, encoding \\ :hex) do
+    # TODO implement using task
+    NodeJS.call({"app", :publicKeyPoints}, [signature, message])
+    |> case do
+      {:ok, %{"publicKey" => pub_points, "signature" => signature_points}} ->
+        public_key = %EllipticCurve.PublicKey.Data{
+          curve: %EllipticCurve.Curve.Data{
+            A: 0,
+            B: 7,
+            G: %EllipticCurve.Utils.Point{
+              x:
+                55_066_263_022_277_343_669_578_718_895_168_534_326_250_603_453_777_594_175_500_187_360_389_116_729_240,
+              y:
+                32_670_510_020_758_816_978_083_085_130_507_043_184_471_273_380_659_243_275_938_904_335_757_337_482_424,
+              z: 1
+            },
+            N:
+              115_792_089_237_316_195_423_570_985_008_687_907_852_837_564_279_074_904_382_605_163_141_518_161_494_337,
+            P:
+              115_792_089_237_316_195_423_570_985_008_687_907_853_269_984_665_640_564_039_457_584_007_908_834_671_663,
+            name: :secp256k1,
+            oid: [1, 3, 132, 0, 10]
+          },
+          point: %EllipticCurve.Utils.Point{
+            x: String.to_integer(pub_points["x"]),
+            y: String.to_integer(pub_points["y"]),
+            z: 1
+          }
+        }
 
+        signature_elixir = %EllipticCurve.Signature.Data{
+          r: String.to_integer(signature_points["r"]),
+          s: String.to_integer(signature_points["s"])
+        }
+
+        EllipticCurve.Ecdsa.verify?(message, signature_elixir, public_key)
+
+      {:error, _} ->
+        false
+    end
   end
 
   @doc """
