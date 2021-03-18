3cda6cb7423fe530f036a216defad54e6ef85f85
venomnert <neerthigan@gmail.com>
2021-03-18 01:58:59 -0400

    Implemented account info fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 import ecc, { PublicKey, Signature } from 'eosjs-ecc'
+import EosApi from 'eosjs-api'
+
+const options = {
+    httpEndpoint: 'https://staging.cambiatus.io',
+    verbose: false
+}
+const eos = EosApi(options)
 
 const publicKeyPoints = (signature, message) => {
     let signatureObj = Signature.from(signature)
@@ -16,5 +23,20 @@ const publicKeyPoints = (signature, message) => {
         }
     }
 }
+const accountToPublicKey = (account) => {
+    const eos = EosApi(options)
+    async function getAccounInfo() {
+        try {
+            return await eos.getAccount(account)
+        } catch (error) {
+            return { error: "error fetching account" };
+        }
+    }
+
+    return (async function () {
+        return await getAccounInfo();
+    })();
+}
 
 exports.publicKeyPoints = publicKeyPoints
+exports.accountToPublicKey = accountToPublicKey
