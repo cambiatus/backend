0244d92b7200ff27020e5d8873996c418d832ed1
venomnert <neerthigan@gmail.com>
2021-03-22 20:54:47 -0400

    formatted code

--- lib/cambiatus_web/subdomain_router.ex
+++ lib/cambiatus_web/subdomain_router.ex
@@ -1,17 +1,15 @@
 defmodule CambiatusWeb.SubdomainRouter do
   use CambiatusWeb, :router
+  use Plug.ErrorHandler
+  use Sentry.Plug
 
   pipeline :api do
     plug(:accepts, ["json"])
   end
 
-  pipeline :browser do
-    plug(:accepts, ["html"])
-  end
-
   scope "/", CambiatusWeb do
-    pipe_through :api
+    pipe_through(:api)
 
-    get "/", Subdomainer.Subdomain.PageController, :index
+    get("/", Subdomainer.Subdomain.PageController, :index)
   end
 end
