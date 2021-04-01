69639e57e1c577edbc65396dae1cf0e68dfb430f
venomnert <neerthigan@gmail.com>
2021-04-01 09:24:13 -0400

    Merged both plugs together

--- lib/cambiatus_web/plugs/set_current_user.ex
+++ lib/cambiatus_web/plugs/set_current_user.ex
@@ -14,17 +14,37 @@ defmodule CambiatusWeb.Plugs.SetCurrentUser do
   def init(opts), do: opts
 
   def call(conn, _) do
-    context = build_context(conn)
+    context_user = set_user(conn)
+    context_phrase = set_phrase(conn)
+    context = Map.merge(context_user, context_phrase)
     Absinthe.Plug.put_options(conn, context: context)
   end
 
-  def build_context(conn) do
+  def set_user(conn) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
          {:ok, %{id: account}} <- AuthToken.verify(token),
          %{} = user <- Cambiatus.Accounts.get_user(account) do
       %{current_user: user}
     else
       _ -> %{}
+    end
+  end
+
+  def set_phrase(conn) do
+    get_req_header(conn, "authorization")
+    |> case do
+      ["Bearer " <> token] ->
+        AuthToken.get_phrase(token)
+        |> case do
+          {:ok, data} ->
+            data |> Map.put(:token, token)
+
+          _ ->
+            %{}
+        end
+
+      [] ->
+        %{}
     end
   end
 end
