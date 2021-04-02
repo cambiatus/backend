e17bfd7e5cbeadcdcbdd5e54e184ecc16561ebd8
Lucca <julienlucca@gmail.com>
2021-04-02 12:04:22 -0300

    Adjust query

--- lib/cambiatus/commune/commune.ex
+++ lib/cambiatus/commune/commune.ex
@@ -196,11 +196,11 @@ defmodule Cambiatus.Commune do
     |> join(:left, [c, a], v in Validator, on: v.action_id == c.action_id)
     |> where(
       [c, a, o, v],
+      # (not (a.usages > 0 and a.usages_left == 0) or fragment("NOW()") < a.deadline) and
       o.community_id == ^community_id and
         v.validator_id == ^account and
         a.is_completed == false and
-        (not (a.usages > 0 and a.usages_left == 0) and
-        # (not (a.usages > 0 and a.usages_left == 0) or fragment("NOW()") < a.deadline) and
+        not (a.usages > 0 and a.usages_left == 0) and
         c.status == "pending" and
         fragment(
           "select count(*) from checks b where b.claim_id = ?.id and b.validator_id = ?",
