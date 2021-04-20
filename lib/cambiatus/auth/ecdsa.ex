fe83c63c3b6a4d0fcfe2de094fc78854e1ec3ba7
venomnert <neerthigan@gmail.com>
2021-04-19 21:34:54 -0400

    removed duplicate functions

--- lib/cambiatus/auth/ecdsa.ex
+++ lib/cambiatus/auth/ecdsa.ex
@@ -8,31 +8,26 @@ defmodule Cambiatus.Auth.Ecdsa do
     with account <- EosWrap.get_account_info(account),
          {:ok, pub_key} <- get_pub_key(account),
          {:ok, result} <- EosWrap.verify(signature, phrase, pub_key) do
-          result
+      result
     else
       {:error, _details} = error -> error
     end
   end
 
-  def sign(signature, priv_key) do
-    EosWrap.sign(signature, priv_key)
-  end
-
-  def sign_with_random(phrase) do
-    EosWrap.gen_rand_signature(phrase)
-  end
-
   def get_pub_key({:ok, %{"error" => err}}), do: {:error, err}
+
   def get_pub_key(account) do
     account
     |> case do
       {:ok, %{"ok" => account_info}} ->
-        pub_key = account_info
-        |> get_in(["permissions", Access.at(0), "required_auth", "keys", Access.at(0), "key"])
+        pub_key =
+          account_info
+          |> get_in(["permissions", Access.at(0), "required_auth", "keys", Access.at(0), "key"])
 
-        {:ok , pub_key}
+        {:ok, pub_key}
 
-      {:error, _} -> {:error, "No public key found"}
+      {:error, _} ->
+        {:error, "No public key found"}
     end
   end
 end
