a463b0013dda2b0f1bf89969142dcf0dca2073f5
Ubuntu <ubuntu@ip-172-31-10-245.ec2.internal>
2021-07-26 14:52:30 +0000

    Add demo to the packaging

--- mix.exs
+++ mix.exs
@@ -114,6 +114,12 @@ defmodule Cambiatus.Mixfile do
         strip_beams: false,
         quiet: false
       ],
+      demo: [
+        include_executables_for: [:unix],
+        include_erts: true,
+        strip_beams: true,
+        quiet: false
+      ],
       cambiatus: [
         include_executables_for: [:unix],
         include_erts: true,
