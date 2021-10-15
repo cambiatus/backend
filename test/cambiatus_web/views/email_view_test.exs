f90ab35f842aca5cb1847b308e699fdede3aead7
ryguigas0 <guigacasa@protonmail.ch>
2021-10-15 16:14:40 -0300

    Updated email view test

--- test/cambiatus_web/views/email_view_test.exs
+++ test/cambiatus_web/views/email_view_test.exs
@@ -6,7 +6,7 @@ defmodule CambiatusWeb.EmailViewTest do
 
   test "renders transfer.html" do
     expected_html =
-      "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n<title>Email recieved</title>\n</head>\n<body>\n  <h1>\nBig title</h1>\n<h2>\nSmall title</h2>\n<h3>\nSmaller title</h3>\n<ul>\n  <li>\nThis is a topic    <ul>\n      <li>\nThis is a subtopic      </li>\n    </ul>\n  </li>\n  <li>\nAnd another one  </li>\n</ul>\n<ul>\n  <li>\nDifferent topic  </li>\n</ul>\n\n</body>\n</html>\n"
+      "<h1>\nBig title</h1>\n<h2>\nSmall title</h2>\n<h3>\nSmaller title</h3>\n<ul>\n  <li>\nThis is a topic    <ul>\n      <li>\nThis is a subtopic      </li>\n    </ul>\n  </li>\n  <li>\nAnd another one  </li>\n</ul>\n<ul>\n  <li>\nDifferent topic  </li>\n</ul>\n"
 
     markdown = """
     # Big title
