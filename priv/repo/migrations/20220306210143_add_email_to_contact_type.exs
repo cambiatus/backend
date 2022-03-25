efc0a824f0159338277161ce19e8e44a7f77f26d
Julien Lucca <julienlucca@gmail.com>
2022-03-25 16:44:11 -0300

    Add directive to old migrations

--- priv/repo/migrations/20220306210143_add_email_to_contact_type.exs
+++ priv/repo/migrations/20220306210143_add_email_to_contact_type.exs
@@ -1,5 +1,6 @@
 defmodule Cambiatus.Repo.Migrations.AddEmailToContactType do
   use Ecto.Migration
+  @disable_ddl_transaction true
 
   def up do
     execute("ALTER TYPE contact_type ADD VALUE 'email'")
--- priv/repo/migrations/20220307130343_add_link_to_contact_type.exs
+++ priv/repo/migrations/20220307130343_add_link_to_contact_type.exs
@@ -1,5 +1,6 @@
 defmodule Cambiatus.Repo.Migrations.AddLinkToContactType do
   use Ecto.Migration
+  @disable_ddl_transaction true
 
   def up do
     execute("ALTER TYPE contact_type ADD VALUE 'link'")
