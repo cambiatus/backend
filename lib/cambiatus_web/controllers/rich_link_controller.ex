75213d659db15198319278397c62f4c57880e776
Matheus da Cunha Buss <bussmatheus@gmail.com>
2022-05-10 23:17:16 -0300

    Updated rich_link to accept markdown. Added pandex

--- lib/cambiatus_web/controllers/rich_link_controller.ex
+++ lib/cambiatus_web/controllers/rich_link_controller.ex
@@ -2,6 +2,7 @@ defmodule CambiatusWeb.RichLinkController do
   use CambiatusWeb, :controller
 
   alias CambiatusWeb.Resolvers.{Accounts, Commune, Shop}
+  require Pandex
 
   @rich_link_file Path.expand("../templates/rich_link.html.eex", __DIR__)
 
@@ -23,11 +24,24 @@ defmodule CambiatusWeb.RichLinkController do
 
     case data do
       {:ok, data} ->
-        response = EEx.eval_file(@rich_link_file, Enum.into(data, []))
-        html(conn, response)
+        response =
+          data
+          |> Enum.map(fn {k, v} -> {k, md_to_txt(v)} end)
+          |> Enum.into([])
+
+        html(conn, EEx.eval_file(@rich_link_file, response))
 
       {:error, reason} ->
         send_resp(conn, 404, reason)
+    end
+  end
+
+  defp md_to_txt(markdown) do
+    with {:ok, string} <- Pandex.markdown_to_plain(markdown) do
+      String.trim(string)
+    else
+      {:error, _} ->
+        ""
     end
   end
 end
