e57de745d4bd90027eff8e70960218863e2e3563
Julien Lucca <julienlucca@gmail.com>
2022-05-14 23:38:18 -0300

    Reorganize files around to better comply with phoenix standard

--- lib/cambiatus/file_uploader.ex
+++ lib/cambiatus/file_uploader.ex
@@ -1,4 +1,4 @@
-defmodule Cambiatus.File.Uploader do
+defmodule Cambiatus.FileUploader do
   @moduledoc "Handles file uploading"
 
   @s3_client Application.compile_env(:cambiatus, :s3_client, ExAws)
