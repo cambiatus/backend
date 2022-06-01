7b81466f6c898e343acb4e2f2f788bb1ed1650f6
Matheus Buss <bussmatheus@gmail.com>
2022-06-01 16:06:43 -0300

    Moved community description to optional fields

--- test/cambiatus/commune/community_test.exs
+++ test/cambiatus/commune/community_test.exs
@@ -5,13 +5,39 @@ defmodule Cambiatus.Commune.CommunityTest do
   use Cambiatus.DataCase
 
   alias Cambiatus.{
-    Commune
+    Commune,
+    Commune.Community
   }
 
   describe "Community Changeset " do
     test "change_community/1 returns a community changeset" do
       community = insert(:community)
       assert %Ecto.Changeset{} = Commune.change_community(community)
+    end
+
+    test "create a valid community changeset with empty values" do
+      params = %{
+        description: "",
+        logo: "",
+        type: "",
+        supply: nil,
+        max_supply: nil,
+        min_balance: nil,
+        issuer: "",
+        website: "",
+        auto_invite: nil,
+        has_objectives: nil,
+        has_shop: nil,
+        has_kyc: nil,
+        highlighted_news: nil
+      }
+
+      changeset =
+        build(:community, params)
+        |> Community.changeset(%{})
+
+      assert true == changeset.valid?
+      assert [] == changeset.errors
     end
   end
 end
