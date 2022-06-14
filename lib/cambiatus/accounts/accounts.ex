f70dd3e4b2fc6dd674d5791f74578c108d0929a5
Matheus Buss <bussmatheus@gmail.com>
2022-06-14 20:14:48 -0300

    Fixed formatting

--- lib/cambiatus/accounts/accounts.ex
+++ lib/cambiatus/accounts/accounts.ex
@@ -72,10 +72,10 @@ defmodule Cambiatus.Accounts do
   @doc """
   Returns the list of users
   ## Examples
-  
+
       iex> list_users()
       [%User{}, ...]
-  
+
   """
   def list_users do
     Repo.all(User)
@@ -83,17 +83,17 @@ defmodule Cambiatus.Accounts do
 
   @doc """
   Gets a single user.
-  
+
   Raises `Ecto.NoResultsError` if the User does not exist.
-  
+
   ## Examples
-  
+
       iex> get_user!(123)
       %User{}
-  
+
       iex> get_user!(456)
       ** (Ecto.NoResultsError)
-  
+
   """
   def get_user!(id), do: Repo.get!(User, id)
   def get_user(id), do: Repo.get(User, id)
@@ -152,15 +152,15 @@ defmodule Cambiatus.Accounts do
 
   @doc """
   Updates a user.
-  
+
   ## Examples
-  
+
   iex> update_user(user, %{field: new_value})
   {:ok, %User{}}
-  
+
   iex> update_user(user, %{field: bad_value})
   {:error, %Ecto.Changeset{}}
-  
+
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -170,15 +170,15 @@ defmodule Cambiatus.Accounts do
 
   @doc """
   Deletes a User.
-  
+
   ## Examples
-  
+
       iex> delete_user(user)
       {:ok, %User{}}
-  
+
       iex> delete_user(user)
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def delete_user(%User{} = user) do
     Repo.delete(user)
@@ -186,12 +186,12 @@ defmodule Cambiatus.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-  
+
   ## Examples
-  
+
   iex> change_user(user)
   %Ecto.Changeset{source: %User{}}
-  
+
   """
   def change_user(params) do
     User.changeset(%User{}, params)
