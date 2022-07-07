e9489824e9805b5cdfc613872311b2cfd564ceac
Matheus Buss <bussmatheus@gmail.com>
2022-07-07 00:06:52 -0300

    Refactored tests

--- test/cambiatus_web/controllers/upload_controller_test.exs
+++ test/cambiatus_web/controllers/upload_controller_test.exs
@@ -2,7 +2,7 @@ defmodule CambiatusWeb.UploadControllerTest do
   use Cambiatus.DataCase
   use CambiatusWeb.ConnCase
 
-  alias Cambiatus.Repo
+  require Poison
 
   @test_image_path Path.join(File.cwd!(), "/test/assets/")
 
@@ -11,17 +11,19 @@ defmodule CambiatusWeb.UploadControllerTest do
   end
 
   describe "Uploads" do
-    setup :valid_community_and_user
-
     test "upload an image",
          %{conn: conn} do
-      filename = "IMG_7095.jpeg"
+      # Send a post request to upload the image "landscape.jpg" and check if 200 is returned
+
+      filename = "landscape.jpg"
 
       upload = %Plug.Upload{
         path: Path.join(@test_image_path, filename),
         filename: filename,
         content_type: "image/jpeg"
       }
+
+      reset_image(upload.path)
 
       conn = post(conn, "/api/upload", %{:file => upload})
 
@@ -30,7 +32,10 @@ defmodule CambiatusWeb.UploadControllerTest do
 
     test "remove metada from an image",
          %{conn: conn} do
-      filename = "white_cube.jpg"
+      # Send a post request to upload the image "portrait.jpg" and check if metadata
+      # has been removed and only allowd metadata was kept
+
+      filename = "portrait.jpg"
 
       upload = %Plug.Upload{
         path: Path.join(@test_image_path, filename),
@@ -38,7 +43,70 @@ defmodule CambiatusWeb.UploadControllerTest do
         content_type: "image/jpeg"
       }
 
+      # Get a list of metadata that is accepted on the final image
+
+      basic_metadata = metadata_whitelist()
+
+      # Resets metadata on the image to be tested
+
+      reset_image(upload.path)
+
+      # Get all the original metadata on the image and check if there's more metadata than
+      # the amount allowed
+
+      {metadata_input, 0} = System.cmd("exiftool", ["-j", upload.path])
+      metadata_input = metadata_input |> Poison.decode!() |> List.first() |> Map.keys()
+
+      assert Enum.count(basic_metadata) < Enum.count(metadata_input)
+
       conn = post(conn, "/api/upload", %{:file => upload})
+
+      # After the image upload, check if only the allowed metadata are present in the output
+
+      {metadata_output, 0} = System.cmd("exiftool", ["-j", upload.path])
+      metadata_output = metadata_output |> Poison.decode!() |> List.first() |> Map.keys()
+
+      assert basic_metadata == metadata_output
+      assert conn.status == 200
     end
+  end
+
+  @doc """
+  This function overwtites an image with its original version. This is done by force copying the
+  intended image with the original copy. For example, the image "foo.jpg" is overwritten by the
+  image "foo_original.jpg".
+  """
+  def reset_image(path) do
+    System.cmd("cp", ["-f", String.replace(path, ".jpg", "_original.jpg"), path])
+  end
+
+  defp metadata_whitelist() do
+    [
+      "BitsPerSample",
+      "ColorComponents",
+      "Directory",
+      "EncodingProcess",
+      "ExifByteOrder",
+      "ExifToolVersion",
+      "FileAccessDate",
+      "FileInodeChangeDate",
+      "FileModifyDate",
+      "FileName",
+      "FilePermissions",
+      "FileSize",
+      "FileType",
+      "FileTypeExtension",
+      "ImageHeight",
+      "ImageSize",
+      "ImageWidth",
+      "MIMEType",
+      "Megapixels",
+      "Orientation",
+      "ResolutionUnit",
+      "SourceFile",
+      "XResolution",
+      "YCbCrPositioning",
+      "YResolution"
+    ]
   end
 end
