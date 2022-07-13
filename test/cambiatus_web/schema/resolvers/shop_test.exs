b7f1a9292b50e1b209668580691c8d6db317d71e
Matheus Buss <bussmatheus@gmail.com>
2022-07-13 17:50:00 -0300

    Fixed shop_test formatting

--- test/cambiatus_web/schema/resolvers/shop_test.exs
+++ test/cambiatus_web/schema/resolvers/shop_test.exs
@@ -464,9 +464,10 @@ defmodule CambiatusWeb.Resolvers.ShopTest do
 
       query_2 = """
       query{
-        products(communityId: "#{community.symbol}", filters: {categories_ids: [#{cat_1.id}, #{
-        cat_2.id
-      }]}) {
+        products(
+          communityId: "#{community.symbol}",
+          filters: {categories_ids: [#{cat_1.id}, #{cat_2.id}]}
+          ) {
           id,
           title
           }
