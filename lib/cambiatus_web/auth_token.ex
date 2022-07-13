943b61e650e63c6de7648691022b1d0b790e33de
Matheus Buss <bussmatheus@gmail.com>
2022-07-13 17:22:05 -0300

    Refactored auth_token

--- lib/cambiatus_web/auth_token.ex
+++ lib/cambiatus_web/auth_token.ex
@@ -5,37 +5,15 @@ defmodule CambiatusWeb.AuthToken do
 
   @doc "Encodes given `user` and signs it, returning a token clients can use as ID"
   def sign(user, authorization \\ "user") do
-    salt =
-      case authorization do
-        "user" ->
-          auth_salt_user()
-
-        "email" ->
-          auth_salt_email()
-      end
-
-    Phoenix.Token.sign(Endpoint, salt, %{id: user.account})
+    Phoenix.Token.sign(Endpoint, auth_salt(authorization), %{id: user.account})
   end
 
   @doc "Decodes original data from given `token` and verifies its integrity"
   def verify(token, authorization \\ "user") do
-    salt =
-      case authorization do
-        "user" ->
-          auth_salt_user()
-
-        "email" ->
-          auth_salt_email()
-      end
-
-    Phoenix.Token.verify(Endpoint, salt, token, max_age: 365 * 24 * 3_600)
+    Phoenix.Token.verify(Endpoint, auth_salt(authorization), token, max_age: 365 * 24 * 3_600)
   end
 
-  def auth_salt_user() do
-    Application.get_env(:cambiatus, :auth_salt)
-  end
+  def auth_salt("user"), do: Application.get_env(:cambiatus, :auth_salt)
 
-  def auth_salt_email() do
-    Application.get_env(:cambiatus, :auth_salt_email)
-  end
+  def auth_salt("email"), do: Application.get_env(:cambiatus, :auth_salt_email)
 end
