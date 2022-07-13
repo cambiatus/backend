b8ec473afd4e364a109aad8d290e49473d78be22
Matheus Buss <bussmatheus@gmail.com>
2022-07-13 14:47:48 -0300

    Added auth_token for emails

--- lib/cambiatus_web/auth_token.ex
+++ lib/cambiatus_web/auth_token.ex
@@ -4,16 +4,38 @@ defmodule CambiatusWeb.AuthToken do
   alias CambiatusWeb.Endpoint
 
   @doc "Encodes given `user` and signs it, returning a token clients can use as ID"
-  def sign(user) do
-    Phoenix.Token.sign(Endpoint, auth_salt(), %{id: user.account})
+  def sign(user, authorization \\ "user") do
+    salt =
+      case authorization do
+        "user" ->
+          auth_salt_user()
+
+        "email" ->
+          auth_salt_email()
+      end
+
+    Phoenix.Token.sign(Endpoint, salt, %{id: user.account})
   end
 
   @doc "Decodes original data from given `token` and verifies its integrity"
-  def verify(token) do
-    Phoenix.Token.verify(Endpoint, auth_salt(), token, max_age: 365 * 24 * 3_600)
+  def verify(token, authorization \\ "user") do
+    salt =
+      case authorization do
+        "user" ->
+          auth_salt_user()
+
+        "email" ->
+          auth_salt_email()
+      end
+
+    Phoenix.Token.verify(Endpoint, salt, token, max_age: 365 * 24 * 3_600)
   end
 
-  def auth_salt() do
+  def auth_salt_user() do
     Application.get_env(:cambiatus, :auth_salt)
+  end
+
+  def auth_salt_email() do
+    Application.get_env(:cambiatus, :auth_salt_email)
   end
 end
