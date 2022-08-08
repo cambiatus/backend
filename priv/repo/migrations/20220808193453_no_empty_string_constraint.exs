a53cf45473bfd0f61deae6f3983d22dbe501624a
Matheus Buss <bussmatheus@gmail.com>
2022-08-08 17:53:52 -0300

    Added documentation to the migration

--- priv/repo/migrations/20220808193453_no_empty_string_constraint.exs
+++ priv/repo/migrations/20220808193453_no_empty_string_constraint.exs
@@ -1,6 +1,18 @@
 defmodule Cambiatus.Repo.Migrations.NoEmptyStringConstraint do
   use Ecto.Migration
 
+  # This migration adds checks to prevent that empty strings are written into the db
+  # The fields affected are
+  #   Objectives.Action.photo_proof_instructions
+  #   Objectives.Action.image
+  #   Objectives.Claim.proof_photo
+  #   Objectives.Claim.proof_code
+  #   Commune.Community.logo
+  #   Commune.Community.description
+  #   Commune.Community.website
+  #   Commune.Transfer.memo
+  #   Shop.Product.description
+
   def change do
     create(constraint("actions", :no_empty_string_on_actions_images, check: "image <> ''"))
 
