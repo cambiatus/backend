580610ea49db2d11d804a002fdddbff795001417
Julien Lucca <julienlucca@gmail.com>
2022-11-25 17:18:55 -0300

    Add missing end

--- test/cambiatus/auth_test.exs
+++ test/cambiatus/auth_test.exs
@@ -138,7 +138,11 @@ defmodule Cambiatus.AuthTest do
 
     test "create_session/1 with invalid data returns error changeset" do
       assert {:error, "Can't parse arguments"} = Auth.create_session(%{})
-      lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+      lorem =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
       assert {:error, "Can't parse arguments"} = Auth.create_session(%{user_agent: lorem})
     end
+  end
 end
